121336523cf35583e010b0621b70c7869e58e27e
Brian Hendrickson <brian.hendrickson@live.com>
2018-11-30 08:18:58 -0800

    finish

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,22 +1,23 @@
 class Timer
 	attr_accessor :seconds
-	def initialize(seconds=0)
-		@seconds = seconds
+
+	def initialize
+		@seconds = 0
 	end
 
 	def time_string
-		hours = 0
-		minutes = 0
-		seconds = @seconds
-		if seconds >= 60 * 60
-			hours = seconds / 60 / 60
-			seconds = seconds - (hours * 60 * 60)	
+		hours = @seconds / 60 / 60
+		minutes = (@seconds - (hours * 60 * 60)) / 60
+		seconds = (@seconds - (hours * 60 * 60)) - minutes * 60
+		"#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"	
+	end	
+
+	def padded(num)
+		num = num.to_s
+		if (num.length) < 2
+			return num = '0' + num
+		else
+			return num
 		end
-		if seconds >= 60
-			minutes = seconds / 60
-			seconds = seconds - minutes * 60	
-		end
-		the_time = Time.new(Time.now.year, Time.now.month, Time.now.day, hours, minutes, seconds)
-		the_time = the_time.strftime('%H:%M:%S')	
 	end
 end
